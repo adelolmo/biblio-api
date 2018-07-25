4c663a7499fa4b0f2be606b120d251a78efd0d74
adelolmo <andoni.delolmo@digitalperformance.de>
2018-07-25 09:05:58 +0200

    upgrade date (de)serializers.

--- src/main/kotlin/org/ado/biblio/infrastructure/DateDeserializer.kt
+++ src/main/kotlin/org/ado/biblio/infrastructure/DateDeserializer.kt
@@ -3,15 +3,14 @@ package org.ado.biblio.infrastructure
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
-import com.fasterxml.jackson.databind.util.ISO8601DateFormat
+import com.fasterxml.jackson.databind.util.StdDateFormat
 import java.io.IOException
 import java.text.ParseException
 import java.util.*
 
 class DateDeserializer(t: Class<Date>) : StdDeserializer<Date>(t) {
 
-    private val df = ISO8601DateFormat()
-
+    private val df = StdDateFormat()
 
     @Throws(IOException::class)
     override fun deserialize(jp: JsonParser, ctxt: DeserializationContext): Date {
--- src/main/kotlin/org/ado/biblio/infrastructure/DateSerializer.kt
+++ src/main/kotlin/org/ado/biblio/infrastructure/DateSerializer.kt
@@ -3,13 +3,13 @@ package org.ado.biblio.infrastructure
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.ser.std.StdSerializer
-import com.fasterxml.jackson.databind.util.ISO8601DateFormat
+import com.fasterxml.jackson.databind.util.StdDateFormat
 import java.io.IOException
 import java.util.*
 
 class DateSerializer(t: Class<Date>) : StdSerializer<Date>(t) {
 
-    private val df = ISO8601DateFormat()
+    private val df = StdDateFormat()
 
     @Throws(IOException::class)
     override fun serialize(value: Date, gen: JsonGenerator, provider: SerializerProvider) {
